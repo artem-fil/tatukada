92ddbf4c27a27b29a97320f85cac6e1ae266ccf3
iamjackswastedlife <iamjacksleastwastedlife@gmail.com>
2016-03-29 13:00:37 +0300

    final

--- public/js/main.js
+++ public/js/main.js
@@ -3,121 +3,131 @@
 
 $(function(){
 
-  $('.info').eq(1).hide();
+  (function(){
+    var $offerCtrlPrev = $('.offer-controls__btn').eq(0);
+    var $offerCtrlNext = $('.offer-controls__btn').eq(1);
+    var $offerPhotos = $('.offer-image-list-li');
+    var $offerTexts = $('.offer-text-list-li');
+    var numOffers = $offerPhotos.length;
+    var prevOffer = numOffers-1;
+    var currentOffer = 0;
+    var nextOffer = 1;
+    var offerSpeed = 500;
 
-  var $offerCtrlPrev = $('.offer-controls__btn').eq(0);
-  var $offerCtrlNext = $('.offer-controls__btn').eq(1);
-  var $offerPhotos = $('.offer-image-list-li');
-  var $offerTexts = $('.offer-text-list-li');
-  var numOffers = $offerPhotos.length;
-  var prevOffer = numOffers-1;
-  var currentOffer = 0;
-  var nextOffer = 1;
-  var offerSpeed = 500;
-
-  $offerCtrlNext.click(function(){
-    $offerTexts.eq(currentOffer).animate({'opacity': '0'}, speed);
-    $offerTexts.eq(nextOffer).animate({'opacity': '1'}, speed);
-    $offerPhotos.eq(currentOffer).animate({'left': '-100%'}, offerSpeed/2, function() {
-      $offerPhotos.eq(nextOffer).animate({'left': '0'}, offerSpeed);
-      prevOffer = currentOffer;
-      currentOffer = nextOffer;
-      nextOffer++;
-      if (currentOffer === numOffers-1) {
-        nextOffer = 0;
-      }
-    });
-  });
-
-  $offerCtrlPrev.click(function(){
-    $offerTexts.eq(currentOffer).animate({'opacity': '0'}, speed);
-    $offerTexts.eq(prevOffer).animate({'opacity': '1'}, speed);
-    $offerPhotos.eq(currentOffer).animate({'left': '-100%'}, offerSpeed/2, function(){
-      $offerPhotos.eq(prevOffer).animate({'left': '0'}, offerSpeed);
-      nextOffer = currentOffer;
-      currentOffer = prevOffer;
-      prevOffer--;
-      if (currentOffer === 0) {
-        prevOffer = numOffers-1;
-      }
-    });
-  });
-
-
-  var $card = $('#card');
-
-  setInterval(function(){
-    $card.hasClass('flipped') ? $card.removeClass('flipped') : $card.addClass('flipped') ;
-  }, 5000);
-
-  var $chefCtrlPrev = $('.chefs-controls__btn').eq(0);
-  var $chefCtrlNext = $('.chefs-controls__btn').eq(1);
-  var $chefsPhotos = $('.chefs-photo-container img');
-  var $chefsCards = $('.chefs-info-list-li');
-  var numChefs = $chefsPhotos.length;
-  var prevChefImage = numChefs-1;
-  var currentChefImage = 0;
-  var nextChefImage = 1;
-  var speed = 800;
-  var currentCard = 1;
-
-  $chefCtrlNext.click(function(){
-
-    $chefsCards.eq(currentChefImage).animate({'opacity': '0'}, speed);
-    $chefsCards.eq(nextChefImage).animate({'opacity': '1'}, speed);
-
-    $chefsPhotos.eq(currentChefImage).animate({'opacity': '0'}, speed);
-    $chefsPhotos.eq(nextChefImage).animate({'opacity': '1'}, speed);
-    prevChefImage = currentChefImage;
-    currentChefImage = nextChefImage;
-    nextChefImage++;
-    if (currentChefImage === numChefs-1) {
-      nextChefImage = 0;
-    }
-  });
-
-  $chefCtrlPrev.click(function(){
-
-    $chefsCards.eq(currentChefImage).animate({'opacity': '0'}, speed);
-    $chefsCards.eq(prevChefImage).animate({'opacity': '1'}, speed);
-
-    $chefsPhotos.eq(currentChefImage).animate({'opacity': '0'}, speed);
-    $chefsPhotos.eq(prevChefImage).animate({'opacity': '1'}, speed);
-    nextChefImage = currentChefImage;
-    currentChefImage = prevChefImage;
-    prevChefImage--;
-    if (currentChefImage === 0) {
-      prevChefImage = numChefs-1;
-    }
-  });
-
-  var $tweetSlider = $('#tweet-slider');
-  var $tweets = $('.twitter-post-list-li');
-  var numTweets = $tweets.length;
-  var currentTweet = 1;
-  var $tweetButtons = $('.twitter-controls__btn');
-  var interval2;
-
-  function startTweetSlider(){
-    interval2 = setInterval(function(){
-      $tweetSlider.animate({'margin-left': '-='+100+'%'}, 800, function(){
-        currentTweet++;
-        $tweetButtons.eq(currentTweet-1).addClass('twitter-controls__btn--active');
-        $tweetButtons.eq(currentTweet-2).removeClass('twitter-controls__btn--active');
-        if(currentTweet === numTweets) {
-          currentTweet = 1;
-          $tweetButtons.eq(0).addClass('twitter-controls__btn--active');
-          $tweetSlider.css('margin-left', 0);
+    $offerCtrlNext.click(function(){
+      $offerTexts.eq(currentOffer).animate({'opacity': '0'}, offerSpeed);
+      $offerTexts.eq(nextOffer).animate({'opacity': '1'}, offerSpeed);
+      $offerPhotos.eq(currentOffer).animate({'left': '-100%'}, offerSpeed/2, function() {
+        $offerPhotos.eq(nextOffer).animate({'left': '0'}, offerSpeed);
+        prevOffer = currentOffer;
+        currentOffer = nextOffer;
+        nextOffer++;
+        if (currentOffer === numOffers-1) {
+          nextOffer = 0;
         }
       });
-    }, 6000);
-    state = true;
-  };
+    });
 
-  startTweetSlider();
+    $offerCtrlPrev.click(function(){
+      $offerTexts.eq(currentOffer).animate({'opacity': '0'}, offerSpeed);
+      $offerTexts.eq(prevOffer).animate({'opacity': '1'}, offerSpeed);
+      $offerPhotos.eq(currentOffer).animate({'left': '-100%'}, offerSpeed/2, function(){
+        $offerPhotos.eq(prevOffer).animate({'left': '0'}, offerSpeed);
+        nextOffer = currentOffer;
+        currentOffer = prevOffer;
+        prevOffer--;
+        if (currentOffer === 0) {
+          prevOffer = numOffers-1;
+        }
+      });
+    });
+  })();
+
+  (function(){
+    var $card = $('#card');
+    setInterval(function(){
+      $card.hasClass('flipped') ? $card.removeClass('flipped') : $card.addClass('flipped') ;
+    }, 5000);
+  })();
+
+  (function(){
+    var $chefCtrlPrev = $('.chefs-controls__btn').eq(0);
+    var $chefCtrlNext = $('.chefs-controls__btn').eq(1);
+    var $chefsPhotos = $('.chefs-photo-container img');
+    var $chefsCards = $('.chefs-info-list-li');
+    var numChefs = $chefsPhotos.length;
+    var prevChefImage = numChefs-1;
+    var currentChefImage = 0;
+    var nextChefImage = 1;
+    var speed = 500;
+    var currentCard = 1;
+
+    $chefCtrlNext.click(function(){
+      $chefsCards.eq(currentChefImage).animate({'opacity': '0'}, speed);
+      $chefsCards.eq(nextChefImage).animate({'opacity': '1'}, speed);
+      $chefsPhotos.eq(currentChefImage).animate({'opacity': '0'}, speed);
+      $chefsPhotos.eq(nextChefImage).animate({'opacity': '1'}, speed);
+      prevChefImage = currentChefImage;
+      currentChefImage = nextChefImage;
+      nextChefImage++;
+      if (currentChefImage === numChefs-1) {
+        nextChefImage = 0;
+      }
+    });
+
+    $chefCtrlPrev.click(function(){
+      $chefsCards.eq(currentChefImage).animate({'opacity': '0'}, speed);
+      $chefsCards.eq(prevChefImage).animate({'opacity': '1'}, speed);
+      $chefsPhotos.eq(currentChefImage).animate({'opacity': '0'}, speed);
+      $chefsPhotos.eq(prevChefImage).animate({'opacity': '1'}, speed);
+      nextChefImage = currentChefImage;
+      currentChefImage = prevChefImage;
+      prevChefImage--;
+      if (currentChefImage === 0) {
+        prevChefImage = numChefs-1;
+      }
+    });
+  })();
+
+  (function(){
+    var $tweetSlider = $('#tweet-slider');
+    var $tweets = $('.twitter-post-list-li');
+    var numTweets = $tweets.length;
+    var currentTweet = 1;
+    var $tweetButtons = $('.twitter-controls__btn');
+
+    function startTweetSlider(){
+      setInterval(function(){
+        $tweetSlider.animate({'margin-left': '-='+100+'%'}, 600, function(){
+          currentTweet++;
+          $tweetButtons.eq(currentTweet-1).addClass('twitter-controls__btn--active');
+          $tweetButtons.eq(currentTweet-2).removeClass('twitter-controls__btn--active');
+          if(currentTweet === numTweets) {
+            currentTweet = 1;
+            $tweetButtons.eq(0).addClass('twitter-controls__btn--active');
+            $tweetSlider.css('margin-left', 0);
+          }
+        });
+      }, 6000);
+      state = true;
+    };
+
+    startTweetSlider();
+  })();
+
+  (function(){
+    var $overlay = $('.video-overlay').eq(0)
+    var video = document.getElementById('video');
+    $('#play__btn').click(function(){
+      $overlay.animate({'opacity': '0'}, 400, function(){
+        $overlay.hide();
+        video.play();
+      });
+    })
+  })();
 
   var width = 100;
-  var animationSpeed = 1100;
+  var animationSpeed = 1000;
   var pause = 5000;
   var $slider = $('#gallery-slider');
   var $slides = $slider.find('.slide');
@@ -195,12 +205,4 @@ $(function(){
     setActive(4);
   })
 
-  var $overlay = $('.video-overlay').eq(0)
-  var video = document.getElementById('video');
-  $('#play__btn').click(function(){
-    $overlay.animate({'opacity': '0'}, 400, function(){
-      $overlay.hide();
-      video.play();
-    });
-  })
 });
